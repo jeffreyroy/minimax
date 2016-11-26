8274f29f78be0af402a19b098a0b826b3a7c63ae
jeffreyroy <jeffrey_a_roy@yahoo.com>
2016-11-26 00:36:38 -0800

    Add method to calculate legal moves

--- annuvin.rb
+++ annuvin.rb
@@ -6,11 +6,26 @@ class Annuvin < Game
   attr_reader :current_state
   attr_accessor :minimax
 
-  INITIAL_BOARD = ["- - . . .",
-                    "- . . . .",
+  # Number of pieces initially on the board
+  NUMBER_OF_PIECES = 4
+
+  # Initial board position
+  INITIAL_BOARD = ["- - O O O",
+                    "- O . . .",
                      ". . . . .",
-                      ". . . . -",
-                       " . . . - -"]
+                      ". . . X -",
+                       "X X X - -"]
+
+  # Characters representing pieces for each player
+  SYMBOLS = { :human => "X", :computer => "O" }
+
+  # Directions on the hex board, for calculating moves
+  DIRECTIONS = { :e =>[0, 1],
+                :w =>[0, -1],
+                :ne =>[-1, 1],
+                :nw =>[-1, 0],
+                :se =>[1, 0],
+                :sw =>[-1, -1] }
 
   ## 1. Methods common to all games, can be redefined if necessary
 
@@ -18,9 +33,19 @@ class Annuvin < Game
   def initialize
     player = :human
     position = init_board
+    # number of pieces left for each player
+    pieces_left = { :human => 4, :computer => 4 }
+    # variables required to record partial move
+    moving_piece = nil
+    moves_left = 0
+    # @current_move_list = []
     # State is a hash consisting of the current position and the
     # Player currently to move
-    @current_state = { :position => position, :player => player }
+    @current_state = {  :position => position,
+                        :player => player,
+                        :pieces_left => pieces_left,
+                        :moving_piece => moving_piece,
+                        :moves_left => moves_left }
   end
 
   # Initialize 3x3 hex board
@@ -31,35 +56,10 @@ class Annuvin < Game
     end
   end
 
-  # Get current position
-  def current_position
-    @current_state[:position]
-  end
-
-  # Get current player
-  def current_player
-    @current_state[:player]
-  end
-
-  # Get opponent of specified player
-  def opponent(player)
-    player == :human ? :computer : :human
-  end
-
-  # Make a move and update the state
-  def make_move(move)
-    @current_state = next_state(@current_state, move)
-  end
-
   # Choose move for computer
   # using minimax
   def computer_move
-    return nil if done?(@current_state)
-    # Pick best move using minimax algorithm
-    move = @minimax.best_move(@current_state)
-    # Make the move
-    display_computer_move(move)
-    make_move(move)
+    get_move
   end
 
   # Use this to calculate score of any position beyond
@@ -73,10 +73,110 @@ class Annuvin < Game
 
   ## 2. Game-specific methods to make moves
 
+  # Helper method for vector addition
+  def add_vector(vector1, vector2)
+    vector1.zip(vector2).map { |x,y| x + y }
+  end
+
+  # Check whether a space is on the board
+  def inbounds?(space)
+    # Return false if outside of array
+    return false if space[0] < 0 || space[0] > 4
+    return false if space[1] < 0 || space[1] > 4
+    # Return false if space marked as off limits
+    return false if self.class::INITIAL_BOARD[space[0]][space[1]] =="-"
+    # Otherwise return true
+    true
+  end
+
   # Legal moves for minimax algorithm
   # Returns array containing list of legal moves in given state
+  # Move is a hash { :destination, :moves_left }
   def legal_moves(state)
-    # Fill this in
+    moves = []
+    position = state[:position]
+    player = state[:player]
+    pieces_left = state[:pieces_left][player]
+    moving_piece = state[:moving_piece]
+    moves_left = state[:moves_left]
+    # If moving piece, add list of additional captures only
+    if moving_piece
+      destinations = get_moves(state, moving_piece, moves_left, true)
+      moves = destinations.map { |destination| [moving_piece, destination] }
+      # Generate list of captures
+      # Add "pass" move
+    else
+      moves_left = state[:moves_left]
+      # Loop through pieces
+      pieces = get_pieces(state)
+      pieces.each do |piece_location|
+        # Add list of moves for this piece
+        destinations = get_moves(state, piece_location, moves_left, false)
+        moves += destinations.map { |destination| [piece_location, destination] }
+      end
+
+
+    end
+    moves
+  end
+
+  # Calculate total number of moves available, given number of pieces left
+  def total_moves(state)
+    player = state[:player]
+    return state[:moves_left] if state[:moving_piece]
+    self.class::NUMBER_OF_PIECES + 1 - state[:pieces_left][player]
+  end
+
+  # Find locations of all of a player's pieces on the board
+  def get_pieces(state)
+    position = state[:position]
+    player = state[:player]
+    symbol = self.class::SYMBOLS[player]
+    pieces = []
+    position.each_with_index do |row, i|
+      row.each_with_index do |space, j|
+        if space == symbol
+          pieces << [i, j]
+        end
+      end
+    end
+    pieces
+  end
+
+  # Get list of possible moves
+  def get_moves(state, start_space, moves_left, capture_only)
+    @current_move_list =[]
+    destinations(state, start_space, moves_left, capture_only)
+    @current_move_list
+  end
+
+  # Find possible destinations for a piece
+  def destinations(state, start_space, moves_left, capture_only)
+    player = state[:player]
+    position = state[:position]
+    directions = self.class::DIRECTIONS
+
+    # Loop through directions
+    directions.each_value do |direction|
+      current_space = add_vector(start_space, direction)
+      # Check whether destination is inbounds
+      if inbounds?(current_space)
+        # p "#{start_space} -> #{current_space}"
+        space_contents = position[current_space[0]][current_space[1]]
+        # Check whether destination is a legal move
+        if space_contents == self.class::SYMBOLS[opponent(player)] || (space_contents == "." && !capture_only)
+          # If move not in move list, add it
+          if !@current_move_list.include?(current_space)
+            @current_move_list << current_space
+          end
+        end
+        # If more moves left, call recursively
+        if moves_left > 1
+          destinations(state, current_space, moves_left - 1, capture_only)
+        end
+      end
+    end
+
   end
 
   # Given state and move, return resulting state after move is made
@@ -94,20 +194,20 @@ class Annuvin < Game
   # Get the player's move and make it
   def get_move
     # Fill this in.  Sample code:
-    # puts
-    # display_position
-    # move = nil
-    # until move != nil
-    #   puts
-    #   print "Enter your move: "
-    #   move_string = gets.chomp
-    #   < interpret move_string as move >
-    #   if !legal_moves(@current_state).index(move)
-    #     puts "That's not a legal move!"
-    #     move = nil
-    #   end
-    # end
-    # make_move(move)
+    puts
+    display_position(@current_state)
+    move = nil
+    until move != nil
+      puts
+      print "Enter your move: "
+      move_string = gets.chomp
+      # < interpret move_string as move >
+      if !legal_moves(@current_state).index(move)
+        puts "That's not a legal move!"
+        move = nil
+      end
+    end
+    make_move(move)
   end
 
   ## 3. Game-specific methods to determine outcome
@@ -115,18 +215,23 @@ class Annuvin < Game
   # Check whether game is over
   def done?(state)
     # Fill this in
+    false
   end
 
   # Check whether game has been won by the player currently on the move
   # in the specified state
   def won?(state)
     # Fill this in
+    player = state[:player]
+    state[:pieces_left][opponent(player)] == 0
   end
 
   # Check whether game has been lost by the player currently on the move
   # in the specified state
   def lost?(state)
     # Fill this in
+    player = state[:player]
+    state[:pieces_left][player] == 0
   end
 
   ## 4. Game-specific displays
@@ -134,6 +239,7 @@ class Annuvin < Game
   # Display the current position
   def display_position(state)
     position = state[:position]
+    player = state[:player]
     count = 0
     position.each do |row|
       print " " * count
@@ -141,7 +247,6 @@ class Annuvin < Game
       puts formatted_row.join(" ")
       count += 1
     end
-    # Fill this in
   end
 
   # Display the computer's move
@@ -157,6 +262,25 @@ end
 # Driver code
 game = Annuvin.new
 
-game.display_position(game.current_state)
 
+done = false
+while !done
+  game_over = false
+  while !game_over
+    game.get_move
+    if game.lost?(game.current_state)
+      puts "You win!!"
+      game_over = true
+    elsif game.done?(game.current_state)
+      puts "Cat's game!"
+      game_over = true
+    else
+      game.computer_move
+      if game.lost?(game.current_state)
+        puts "I win!" 
+        game_over = true
+      end
+    end
+  end
+end
 
